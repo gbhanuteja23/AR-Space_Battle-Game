11d8f171b60b3e7302652358485f110a766c2e50
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-07-23 12:16:20 +0530

    change_18

---
+++ AR Space Battle Game/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //for this to work both need colliders, one must have rigid body (spaceship) the other must have is trigger checked.
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject explosion = Instantiate(Resources.Load("FlameThrower Variant", typeof(GameObject))) as GameObject;
+        explosion.transform.position = transform.position;
+        Destroy(col.gameObject);
+        Destroy(explosion, 2);
+
+
+        if (GameObject.FindGameObjectsWithTag("Player").Length == 0)
+        {
+
+            GameObject enemy = Instantiate(Resources.Load("enemy", typeof(GameObject))) as GameObject;
+            GameObject enemy1 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+            GameObject enemy2 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+            GameObject enemy3 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+
+        }
+
+        Destroy(gameObject);
+
+
+    }
+
+}
