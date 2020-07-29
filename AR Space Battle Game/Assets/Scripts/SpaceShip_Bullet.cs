55d534409ecf7f05ab10db14e9c0ea713e29400f
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-07-29 23:55:20 +0530

    change_25

--- AR Space Battle Game/Assets/Scripts/SpaceShip_Bullet.cs
+++ AR Space Battle Game/Assets/Scripts/SpaceShip_Bullet.cs
@@ -11,6 +11,8 @@ public class SpaceShip_Bullet : MonoBehaviour
     {
 
         StartCoroutine("Move");
+
+        //StartCoroutine("Shoot");
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class SpaceShip_Bullet : MonoBehaviour
 
     }
 
+    /*
     void ShootshipBullets()
     {
         GameObject bullet = Instantiate(Resources.Load("bullet", typeof(GameObject))) as GameObject;
@@ -31,22 +34,25 @@ public class SpaceShip_Bullet : MonoBehaviour
         //Vector3 position_val = transform.position;
         //Quaternion rotation_val = transform.rotation;
 
-        /*float x = transform.position.x;
+        float x = transform.position.x;
         float y = transform.position.y;
         float z = transform.position.z;
 
         Instantiate(Resources.Load("bullet", typeof(GameObject)), new Vector3(x,y,z), Quaternion.identity);
-        */
+        
 
         //Vector3 position = GameObject.Find("SF_Free-Fighter Variant").transform.position;
 
 
         
-        rb.AddRelativeForce(transform.forward * 500f);
+        rb.AddForce(transform.forward * 500f);
         Destroy(bullet, 3);
 
         GetComponent<AudioSource>().Play();
+
     }
+    
+*/
 
     IEnumerator Move()
     {
@@ -56,7 +62,24 @@ public class SpaceShip_Bullet : MonoBehaviour
         {
             yield return new WaitForSeconds(3.5f);
             transform.eulerAngles += new Vector3(0, 180f, 0);
+            
+        }
+    }
+
+    /*
+
+    IEnumerator Shoot()
+    {
+
+
+        while (true)
+        {
+            yield return new WaitForSeconds(3.5f);
+            
             ShootshipBullets();
         }
     }
+
+    */
+
 }
