e0602d8a8c21302691c9025cfbe1876e66a982d3
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-07-30 20:11:16 +0530

    change_9

---
+++ AR Space Battle Game/Assets/Scripts/ShowScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowScore : MonoBehaviour
+{
+    public int score_count = 0;  //To count the no of enemy spaceships destroyed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        score_count = score_count + 1;    //Whenever you destroy a enemy spaceship, increase score by 1
+    }
+}
