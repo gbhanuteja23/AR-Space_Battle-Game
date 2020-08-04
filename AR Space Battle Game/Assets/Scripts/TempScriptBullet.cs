7a1104e14bbdca6bcdba01b0326d59e8347da59c
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-08-04 13:39:52 +0530

    change_21

---
+++ AR Space Battle Game/Assets/Scripts/TempScriptBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempScriptBullet : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+
+    public void ShootBullet()
+    {
+        rb.AddForce(rb.transform.position*500f);
+    }
+}
