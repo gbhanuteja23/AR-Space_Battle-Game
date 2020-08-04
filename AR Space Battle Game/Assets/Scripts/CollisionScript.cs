e0bb3a3ea6f951047dcc11307d55b42fb1dc3fdf
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-08-04 13:39:22 +0530

    change_16

--- AR Space Battle Game/Assets/Scripts/CollisionScript.cs
+++ AR Space Battle Game/Assets/Scripts/CollisionScript.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class CollisionScript : MonoBehaviour
-{
-    
+{    
 
     // Use this for initialization
     void Start()
@@ -17,14 +16,22 @@ public class CollisionScript : MonoBehaviour
         
     }
 
+    
     //for this to work both need colliders, one must have rigid body (spaceship) the other must have is that "istrigger" should be checked.
     void OnTriggerEnter(Collider col)
-    {
+    {        
+
         GameObject explosion = Instantiate(Resources.Load("BigExplosion Variant", typeof(GameObject))) as GameObject;
         explosion.transform.position = transform.position;
         Destroy(col.gameObject);
-        Destroy(explosion, 3);       
-       
+        Destroy(explosion, 3);
+
+        if(col.gameObject.tag=="Player")
+        {
+            ShowScore.score_count += 1;
+        }
+        
+
 
         Destroy(gameObject);
     }
