1ff6479db7969e30c7cdd2ed1d4552340e3c8d03
gbhanuteja23 <g.bhanuteja23@gmail.com>
2020-08-04 13:39:35 +0530

    change_18

---
+++ AR Space Battle Game/Assets/Scripts/ScoreUpdateOnCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUpdateOnCollision : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.transform.name=="Player")
+        {
+            Debug.Log("Collided");
+            ShowScore.score_count += 1;
+        }
+    }
+}
